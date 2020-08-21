6a70148c09eed30519e51b7ef5f89939de049a0a
Mr-Sencer <husencerdiba@gmail.com>
2020-08-22 02:38:12 +0300

    Add files via upload

---
+++ SearchResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessingLib
+{
+    public struct SearchResult : IDisposable
+    {
+        //aranılan karakter :_char
+        public char _char;
+        public int İndex;
+        public SearchResult(char c, int index, bool found)
+        {
+            _char = c;
+
+            İndex = index;
+            Found = found;
+        }
+        public bool Found;
+        public void Dispose()
+        {
+            GC.SuppressFinalize(_char);
+            GC.SuppressFinalize(İndex);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
